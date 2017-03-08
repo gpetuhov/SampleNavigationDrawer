75aeac080c025674997a96c6436fe712c2875ab6
Gennadiy Petuhov <g3pmail@gmail.com>
2017-03-08 13:12:14 +0400

    Handle menu items clicks

--- app/src/main/java/com/gpetuhov/android/samplenavigationdrawer/MainActivity.java
+++ app/src/main/java/com/gpetuhov/android/samplenavigationdrawer/MainActivity.java
@@ -3,7 +3,10 @@ package com.gpetuhov.android.samplenavigationdrawer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,6 +14,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Drawer layout
+    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
 
     // ListView for menu items
     @BindView(R.id.left_drawer) ListView mDrawerList;
@@ -26,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         // Bind views
         ButterKnife.bind(this);
 
+        // Add first item fragment to activity
+        loadFragment(0);
+
         // Get menu item titles
         mMenuTitles = getResources().getStringArray(R.array.menu_item_titles);
 
@@ -33,14 +42,28 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList.setAdapter(new ArrayAdapter<>(this,
                 R.layout.drawer_list_item, mMenuTitles));
 
-        // Add first item fragment to activity
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+    }
+
+    private void loadFragment(int position) {
         FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.content_frame);
-        if (null == fragment) {
-            fragment = ItemFragment.newInstance(0);
-            fm.beginTransaction()
-                    .add(R.id.content_frame, fragment)
-                    .commit();
-        }
+        Fragment fragment = ItemFragment.newInstance(position);
+        fm.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+    }
+
+    private void selectItem(int position) {
+        // Add fragment to activity according to selected position
+        loadFragment(position);
+
+        // Highlight the selected item and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
 }
